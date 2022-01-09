891a96d64d17a869cbb8c401130bd721393d9060
Johnson Iyida <kodili.johnson@gmail.com>
2022-01-09 20:48:51 +0100

    configure ingress for app

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,9 +4,9 @@ const app = express();
 app.use(json());
 
 app.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there !!!');
-});
+    res.send('Hi there. Am testing this!!!')
+})
 
 app.listen(3000, () => {
-    console.log(`listening on port 3000! for real`);
+    console.log(`listening on port 3000!`);
 });

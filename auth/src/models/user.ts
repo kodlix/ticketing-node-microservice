fc3d28f8ddd829943c3e2ae95800c09b5c8793c7
Johnson Iyida <kodili.johnson@gmail.com>
2022-01-22 10:11:52 +0100

    remove password and __v from signup response

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -30,6 +30,15 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+}, {
+    toJSON: {
+        transform(doc, ret){
+            ret.id = ret._id;
+            delete ret._id;
+            delete ret.__v;
+            delete ret.password;
+        }
+    }
 })
 
 UserSchema.pre('save',async function (done) {

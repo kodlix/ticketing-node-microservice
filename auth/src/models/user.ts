c51040185510bad57a0150b51d7d0a65c4991e86
Johnson Iyida <kodili.johnson@gmail.com>
2022-01-22 08:11:15 +0100

    implement user sign up

---
+++ auth/src/models/user.ts
@@ -0,0 +1,49 @@
+import mongoose, { Mongoose } from 'mongoose';
+import { Password } from '../services/password';
+
+//An interface that describes the properties
+//required to create a new user
+interface IUserAttrs {
+    email: string;
+    password: string;
+};
+
+//An interface that describes the properties
+//that a User Model has
+interface IUserModel extends mongoose.Model<IUserDoc> {
+   build(attrs: IUserAttrs): IUserDoc;
+};
+
+//An interface that describes the properties
+//a User Document has
+interface IUserDoc extends mongoose.Document {
+    email: string;
+    password: string;
+};
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+UserSchema.pre('save',async function (done) {
+   if (this.isModified('password')) {
+       const hashed = await Password.toHash(this.get('password'));
+       this.set('password', hashed);
+   }
+   done();
+});
+
+UserSchema.statics.build = (attrs: IUserAttrs) =>{
+    return new User(attrs);
+}
+
+const User = mongoose.model<IUserDoc, IUserModel>('User', UserSchema);
+
+export { User };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { BadRequestError } from "../errors/bad-request-error";
 import { DatabaseConnectionError } from "../errors/database-connection-error";
 import { RequestValidationError } from "../errors/request-validation-error";
+import { User } from "../models/user";
 
 const router = express.Router();
 
@@ -14,20 +16,22 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
     const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      throw new BadRequestError("User email already exists");
+    }
 
-    console.log("Creating a user...");
-    throw new DatabaseConnectionError();
-    
-    res.send({});
+    const newUser = User.build({ email, password });
+    await newUser.save();
 
-    // new User({ email, password })
+    res.status(201).send(newUser);
   }
 );
 
